80f20f936ebf7a2c5bd2eb0c2bf61c93a7446e48
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2024-09-16 10:37:38 -0300

    add while, switch and case to the function start

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
 function start() {
 
+    while (true) {
+
+        let opcao = "cadastrar"
+
+        switch (opcao) {
+
+            case "cadastrar":
+
+                console.log("Vamos cadastrar");
+
+                break
+
+            case "listar":
+
+                console.log("Vamos listar");
+
+                break
+
+            case "sair":
+
+                return
+
+
+        }
+    }
 }
 
 start()
